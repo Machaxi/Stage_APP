9af39c6aba817f440e6ab0acf51b76072e2e39cb
auriga <auriga@Suryas-MBP.aurigait.com>
2019-10-11 15:14:54 +0530

    opponent search height fix

--- app/containers/challenge/OpponentList.js
+++ app/containers/challenge/OpponentList.js
@@ -530,6 +530,7 @@ const styles = StyleSheet.create({
         elevation: 1
     },
     searchBox: {
+        height:40,
         marginLeft: 8,
         backgroundColor: 'white',
         borderRadius: 16,
