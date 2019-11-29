230aab42f05f14abaec4be15ef3c5b3f083c247b
rajat-galav <rajatgalav1295@gmail.com>
2019-11-29 17:21:42 +0530

    progress per session

---
+++ app/containers/CoachScreen/Batch/ProgressPerSession.js
@@ -0,0 +1,142 @@
+
+import React from 'react'
+import { View, ImageBackground, Text, StyleSheet, Image, TouchableOpacity, Dimensions, ActivityIndicator, FlatList, ScrollView } from 'react-native';
+import { connect } from 'react-redux';
+import BaseComponent, { defaultStyle } from '../../BaseComponent'
+
+const acedemicList = [
+    {
+        label: 'India',
+        value: 'IN',
+    }
+
+];
+
+const placeholder = {
+    label: 'Select Option',
+    value: null,
+    color: '#9EA0A4',
+};
+var deviceWidth = Dimensions.get('window').width - 20;
+
+class ProgressPerSession extends BaseComponent {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+            batchList: null,
+            userData: null,
+            isRefreshing: false
+
+        }
+    }
+
+
+    
+
+
+    render() {
+        return (
+            <View style={{ flex: 1, marginTop: 0, backgroundColor: '#F7F7F7' }}>
+
+            </View>  )
+    }
+
+
+}
+const mapStateToProps = state => {
+    return {
+        data: state.BatchReducer,
+    };
+};
+const mapDispatchToProps = {
+};
+export default connect(mapStateToProps, mapDispatchToProps)(ProgressPerSession);
+
+
+const pickerSelectStyles = StyleSheet.create({
+    inputIOS: {
+        fontSize: 16,
+        // paddingVertical: 12,
+        //paddingHorizontal: 10,
+        borderWidth: 0,
+        borderColor: '#D3D3D3',
+        borderRadius: 4,
+        color: 'white',
+        // paddingLeft: 10,
+
+        // alignItems: 'stretch',
+        // // justifyContent: 'right',
+        alignSelf: 'center',
+        height: 40,
+        marginRight: 10,
+        marginTop: 5,
+        marginBottom: 5
+        // to ensure the text is never behind the icon
+    },
+    inputAndroid: {
+        fontSize: 16,
+        paddingHorizontal: 10,
+        paddingVertical: 8,
+        borderWidth: 0.5,
+        borderColor: '#614051',
+        borderRadius: 8,
+        color: 'black',
+        paddingRight: 30, // to ensure the text is never behind the icon
+    },
+});
+
+const styles = StyleSheet.create({
+    navBar: {
+        height: 60,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        // backgroundColor: 'blue',
+    },
+    leftContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        // backgroundColor: 'green'
+    },
+    rightContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        // backgroundColor: 'red',
+    },
+    rightIcon: {
+        height: 25,
+        width: 25,
+        resizeMode: 'contain',
+        marginRight: 20
+        //backgroundColor: 'white',
+    },
+
+    scoreBox: {
+        color: 'white',
+        marginRight: 20,
+        textAlign: 'right', fontSize: 24, fontWeight: 'bold'
+    },
+    buttomButton: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        height: 45,
+
+        backgroundColor: 'white',
+        marginTop: 10,
+        marginBottom: -5,
+        marginLeft: -5,
+        marginRight: -5,
+        shadowColor: 'black',
+        shadowOpacity: 0.5,
+        shadowOffset: { width: 0, height: 1 }, borderBottomRightRadius: 10, borderBottomLeftRadius: 10
+
+    }
+
+
+});
